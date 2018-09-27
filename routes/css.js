ac3aa79fa65622334a0f277fb4b091bb9fae26db
Ondrej Salamon <ondrej.salamon@digitalbox.cz>
2018-09-27 21:54:16 +0200

    started adding some routes for user, esp. his profile

--- routes/css.js
+++ routes/css.js
@@ -1,6 +1,8 @@
 const express = require('express');
 var router = express.Router;
 
+// I didnt make separate route/file csslabels.js/; it will be managed by this route, e.g of URL '/css/labels', i.e. cssLabelcontrollers, e.g. list_css_labels, css_label_create_get etc
+
 router.get('/', (req, res) => {
   // res.send ... = css controller
 })
